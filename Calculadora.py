d0bb86cee60f6c3114afca0a7e7666dc0c8ddea4
Jorge Dantas <jatad.tech@gmail.com>
2024-09-10 18:45:09 -0300

    refactor: Troca de posicoes

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,4 @@
+
 def add(x, y):
     return x + y
   
@@ -12,29 +13,27 @@ def divide(x, y):
         return x / y
     else:
         return "Erro! Divisão por zero."
-      
-def main():
-    print("Selecione a operação:")
-    print("1. Adição")
-    print("2. Subtração")
-    print("3. Multiplicação")
-    print("4. Divisão")
 
-    choice = input("Digite o número da operação (1/2/3/4): ")
+print("Selecione a operação:")
+print("1. Adição")
+print("2. Subtração")
+print("3. Multiplicação")
+print("4. Divisão")
 
-    if choice in ['1', '2', '3', '4']:
-        num1 = float(input("Digite o primeiro número: "))
-        num2 = float(input("Digite o segundo número: "))
-    if choice == '1':
-            print(f"{num1} + {num2} = {add(num1, num2)}")
-    elif choice == '2':
-            print(f"{num1} - {num2} = {subtract(num1, num2)}")
-    elif choice == '3':
-            print(f"{num1} * {num2} = {multiply(num1, num2)}")
-    elif choice == '4':
-            print(f"{num1} / {num2} = {divide(num1, num2)}")
-    else:
-        print("Entrada inválida.")
-  
-    if __name__ == "__main__":
-      main()
+choice = input("Digite o número da operação (1/2/3/4): ")
+
+if choice in ['1', '2', '3', '4']:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+if choice == '1':
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif choice == '2':
+    print(f"{num1} - {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"{num1} * {num2} = {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"{num1} / {num2} = {divide(num1, num2)}")
+else:
+    print("Entrada inválida.")
+
+
